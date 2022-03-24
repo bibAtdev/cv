8cdd75c3dcbd718de383472a260bdd0b35b3a055
Andreas Bib <bibatdevelopment@gmail.com>
2022-03-24 22:06:13 +0100

    Inital commit

---
+++ scripts/deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+## Ref.: https://cli.vuejs.org/guide/deployment.html#github-pages
+
+# abort on errors
+set -e
+
+cd ..
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com/bibAtdev/cv.git main:gh-pages
+
+cd -
+sleep 20s

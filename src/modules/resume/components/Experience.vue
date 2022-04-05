cbb1f0b9615f920a857debcd42bd536064a76177
Andreas Bib <bibatdevelopment@gmail.com>
2022-04-05 21:30:45 +0200

    Added tryout of d3.js

--- src/modules/resume/components/Experience.vue
+++ src/modules/resume/components/Experience.vue
@@ -31,13 +31,51 @@
             </SubItem>
         </div>
     </item>
+    
+    <div>
+        <div class="container-chart text-align-left">
+        <div class="chart"></div>
+        </div>
+    </div>
+
 </template>
 <script>
 import Item from "@/modules/resume/components/Item"
 import SubItem from "@/modules/resume/components/SubItem"
+import * as d3 from 'd3'
 
 export default {
     name: 'Experience-Component',
-    components: {Item, SubItem}
+    components: {Item, SubItem},
+    data () {
+        return {}
+    },
+    mounted () {
+        let data = [4, 8, 15, 16, 23, 42]
+        d3.select('.chart')
+        .selectAll('div')
+        .data(data)
+        .enter().append('div')
+        .style('width', function (d) { return d * 10 + 'px' })
+        .text(function (d) { return d })
+    },
 }
 </script>
+
+<style scoped>
+</style>
+
+<style>
+.chart div {
+  font: 10px sans-serif;
+  background-color: steelblue;
+  text-align: right;
+  padding: 3px;
+  margin: 1px;
+  color: white;
+}
+.container-chart {
+  /* width: 50%; */
+  margin: auto;
+}
+</style>
